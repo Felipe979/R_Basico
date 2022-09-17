bc49fa95b36217672016560fd3bda800994345f6
Felipe979 <felipeferreira0090@gmail.com>
2022-09-17 10:59:55 -0300

    Teste git comand line - cli

---
+++ Aula 1 - Instalação de ferramentas.R
@@ -0,0 +1,18 @@
+### Instalando os pacotes necessários para trabalhar no software durante o curso
+
+install.packages(c("ggplot2","swirl","lattice","car","fImport","tseries"))
+
+# Carregando os pacotes para utilização nesses script
+
+library(ggplot2)
+library(swirl)
+library(lattice)
+library(car)
+library(fImport)
+library(tseries)
+
+# Uma maneira alternativa de realizar esse conjunto de instalações é:
+
+#if(!require(pacman)){install.packages("pacman")}#
+#pacman::p_load(ggplot2, swirl, lattice, car, fImport, tseries)#
+
